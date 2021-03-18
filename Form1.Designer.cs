586fc178ffb21d18d4dad156cf740fac62c06afb
Angelo M <70480326+AngeloMetal@users.noreply.github.com>
2021-03-18 16:59:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace CoinFlippedSeed
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.header = new System.Windows.Forms.Label();
+            this.heads = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timesLabel = new System.Windows.Forms.Label();
+            this.tails = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.AutoSize = true;
+            this.header.Location = new System.Drawing.Point(12, 43);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(175, 75);
+            this.header.TabIndex = 0;
+            this.header.Text = "No random number generators.\r\nJust flip a coin 128 times.\r\nThis will be your entr" +
+    "opy.\r\nHeads = 1\r\nTails = 0";
+            // 
+            // heads
+            // 
+            this.heads.Location = new System.Drawing.Point(257, 57);
+            this.heads.Name = "heads";
+            this.heads.Size = new System.Drawing.Size(80, 33);
+            this.heads.TabIndex = 1;
+            this.heads.Text = "Heads";
+            this.heads.UseVisualStyleBackColor = true;
+            this.heads.Click += new System.EventHandler(this.heads_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 299);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(745, 122);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(745, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timesLabel
+            // 
+            this.timesLabel.AutoSize = true;
+            this.timesLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timesLabel.Location = new System.Drawing.Point(377, 23);
+            this.timesLabel.Name = "timesLabel";
+            this.timesLabel.Size = new System.Drawing.Size(64, 28);
+            this.timesLabel.TabIndex = 4;
+            this.timesLabel.Text = "0/128";
+            // 
+            // tails
+            // 
+            this.tails.Location = new System.Drawing.Point(483, 57);
+            this.tails.Name = "tails";
+            this.tails.Size = new System.Drawing.Size(80, 33);
+            this.tails.TabIndex = 5;
+            this.tails.Text = "Tails";
+            this.tails.UseVisualStyleBackColor = true;
+            this.tails.Click += new System.EventHandler(this.tails_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mnemonic:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Derived addresses:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(30, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 19);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Legacy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(30, 76);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(100, 19);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "Nested Segwit";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(30, 51);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(96, 19);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "Native segwit";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(629, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(159, 203);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 25);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(30, 127);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(99, 19);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Show entropy";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(30, 102);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 19);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Show private keys";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(30, 430);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 11);
+            this.label3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tails);
+            this.Controls.Add(this.timesLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.heads);
+            this.Controls.Add(this.header);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Coin Flipped Seed";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label header;
+        private System.Windows.Forms.Button heads;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button tails;
+        private System.Windows.Forms.Label timesLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NBitcoin;
+
+namespace CoinFlippedSeed
+{
+    public partial class Form1 : Form
+    {
+        public string bins = "";
+        public int times = 0;
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void heads_Click(object sender, EventArgs e)
+        {
+            Check("heads");
+
+        }
+
+        private void tails_Click(object sender, EventArgs e)
+        {
+            Check("tails");
+        }
+
+        public void Check(string t)
+        {
+            if(t == "heads")
+            {
+                bins += "1";
+            }else if(t == "tails")
+            {
+                bins += "0";
+            }
+
+            if(checkBox2.Checked == true)
+            {
+                label3.Text = "Entropy: " + bins;
+            }
+
+            times += 1;
+            timesLabel.Text = times + "/128";
+
+            if(bins.Length == 128)
+            {
+                heads.Enabled = false;
+                tails.Enabled = false;
+                Finalize(bins);
+            }
+        }
+
+        public void Finalize(string input)
+        {
+            //Finalize("01101101011100100001010101101000101010010100000010010001001001110000100111100010101000101010010101000000001010110110001001010011");
+            int numOfBytes = input.Length / 8;
+            byte[] bytes = new byte[numOfBytes];
+            for (int i = 0; i < numOfBytes; ++i)
+            {
+                bytes[i] = Convert.ToByte(input.Substring(8 * i, 8), 2);
+            }
+            Mnemonic mnem = new Mnemonic(Wordlist.English, bytes);
+            
+            textBox1.Text = mnem.ToString();
+            ExtKey hdroot = mnem.DeriveExtKey();
+            int addressType = 0;
+            ScriptPubKeyType typeofAddress = ScriptPubKeyType.Segwit;
+            if (radioButton1.Checked == true)
+            {
+                addressType = 44;
+                typeofAddress = ScriptPubKeyType.Legacy;
+            }
+            else if(radioButton2.Checked == true)
+            {
+                addressType = 49;
+                typeofAddress = ScriptPubKeyType.SegwitP2SH;
+            }
+            else if(radioButton3.Checked == true)
+            {
+                addressType = 84;
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                
+                var pKey = hdroot.Derive(new KeyPath("m/" + addressType + "'/0'/0'/0/" + i));
+                var address = pKey.PrivateKey.PubKey.GetAddress(typeofAddress, Network.Main);
+                if(checkBox1.Checked == false)
+                {
+                    richTextBox1.Text += address.ToString() + "\n";
+                }
+                else
+                {
+                    richTextBox1.Text += address.ToString() + "," + pKey.PrivateKey.GetWif(Network.Main) + "\n";
+                }
+                
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //on change "Show private keys"
+           
+            richTextBox1.Text = "";
+            if (bins.Length == 128)
+            {
+                Finalize(bins);
+            }
+           
+            
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //on change "Show entropy"
+            if(checkBox2.Checked == false)
+            {
+                label3.Text = "";
+            }
+            else
+            {
+                label3.Text = "Entropy: " + bins;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            heads.Enabled = true;
+            tails.Enabled = true;
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+            bins = "";
+            timesLabel.Text = "0/128";
+            label3.Text = "Entropy: ";
+            times = 0;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            if(bins.Length == 128)
+            {
+                Finalize(bins);
+            }
+            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            if (bins.Length == 128)
+            {
+                Finalize(bins);
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            if (bins.Length == 128)
+            {
+                Finalize(bins);
+            }
+        }
+    }
+}
